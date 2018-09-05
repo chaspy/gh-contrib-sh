07c8a7c39aafaed21b9d810e65bc2d621ad57d24
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-05 22:56:35 +0900

    Make url variable

--- gh-contrib.sh
+++ gh-contrib.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -x
+
+readonly URL="https://api.github.com/search/issues"
+
 AUTHOR="chaspy"
 TERMS="2018-06-21..2018-07-21"
 
@@ -8,7 +11,7 @@ if [[ $TOKEN != "" ]]; then
   AUTH="Authorization: token ${TOKEN}"
 fi
 
-JSON=`curl -sS -H "$AUTH" "https://api.github.com/search/issues?q=type:pr+in:body+is:merged+merged:${TERMS}+author:${AUTHOR}&per_page=100"`
+JSON=`curl -sS -H "$AUTH" "${URL}?q=type:pr+in:body+is:merged+merged:${TERMS}+author:${AUTHOR}&per_page=100"`
 NUM=`echo $JSON | jq ".items[].title" | wc -l`
 PR=`echo $JSON | jq -c '.items[] | {title,html_url} | .html_url |= . + "\n" ' \
                | jq -c 'sort_by(.html_url) | .[]' --slurp \
