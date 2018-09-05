e2a7028d71d0c0e39cfd820dcba7df3dedc6f9e6
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-05 23:31:12 +0900

    getopts

--- gh-contrib.sh
+++ gh-contrib.sh
@@ -3,21 +3,49 @@ set -x
 
 readonly URL="https://api.github.com/search/issues"
 
-AUTHOR="chaspy"
-TERMS="2018-06-21..2018-07-21"
+usage() {
+  cat <<EOS >&2
+Usage: $0 -u <user> -s YYYY-MM-DD -e YYYY-MM-DD
+  -u: your github user name
+  -s: start date to get your contribution
+  -e: end date to get your contribution
+EOS
+  exit 1
+}
 
-source ./token.sh
-if [[ $TOKEN != "" ]]; then
-  AUTH="Authorization: token ${TOKEN}"
+parse_args() {
+  while getopts "u:s:e:" OPT; do
+    case $OPT in
+      u) AUTHOR="$OPTARG" ;;
+      s) START="$OPTARG" ;;
+      e) END="$OPTARG" ;;
+      :) echo  "[ERROR] Option argument is undefined.";;
+      \?) echo "[ERROR] Undefined options.";;
+    esac
+  done
+
+  shift $((OPTIND - 1))
+}
+
+main() {
+  source ./token.sh
+  if [[ $TOKEN != "" ]]; then
+    AUTH="Authorization: token ${TOKEN}"
+  fi
+
+  JSON=`curl -sS -H "$AUTH" "${URL}?q=type:pr+in:body+is:merged+merged:${TERMS}+author:${AUTHOR}&per_page=100"`
+  NUM=`echo $JSON | jq ".items[].title" | wc -l`
+  PR=`echo $JSON | jq -c '.items[] | {title,html_url} | .html_url |= . + "\n" ' \
+                 | jq -c 'sort_by(.html_url) | .[]' --slurp \
+                 | jq -r .[]`
+
+  echo "Hi ${AUTHOR}, this is your contribution report :tada: in $TERMS"
+  echo "# Pull Request"
+  echo "your created and merged pull request is ${NUM}!!"
+  echo -n "$PR"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  parse_args "$@"
+  main
 fi
-
-JSON=`curl -sS -H "$AUTH" "${URL}?q=type:pr+in:body+is:merged+merged:${TERMS}+author:${AUTHOR}&per_page=100"`
-NUM=`echo $JSON | jq ".items[].title" | wc -l`
-PR=`echo $JSON | jq -c '.items[] | {title,html_url} | .html_url |= . + "\n" ' \
-               | jq -c 'sort_by(.html_url) | .[]' --slurp \
-               | jq -r .[]`
-
-echo "Hi ${AUTHOR}, this is your contribution report :tada: in $TERMS"
-echo "# Pull Request"
-echo "your created and merged pull request is ${NUM}!!"
-echo -n "$PR"
